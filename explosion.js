3a667814a79f5a6a9314f33db9392b72ad4a27b4
Ken Gil Romero <kenromero12345@gmail.com>
2020-02-20 22:59:46 -0800

    Update explosion.js

--- explosion.js
+++ explosion.js
@@ -6,21 +6,24 @@ function Explosion(game, x, y) {
     this.speed = 200;
     this.x = x;
     this.y = y;
+    this.width = this.animation.frameWidth;
+    this.height = this.animation.frameHeight;
     this.boxes = true;
-    this.boundingbox = new BoundingBox(this.x, this.y, this.width, this.height);
+    this.boundingbox = new BoundingBox(this.x + 25, this.y + 22, this.width - 50, this.height - 50);
 }
 
 Explosion.prototype.draw = function() {
     if (this.animation.isDone()) {
         this.removeFromWorld = true;
+    } else {
+        if (this.boxes) {
+            this.ctx.strokeStyle = "red";
+            this.ctx.strokeRect(this.x, this.y, this.animation.frameWidth, this.animation.frameHeight);
+            this.ctx.strokeStyle = "green";
+            this.ctx.strokeRect(this.boundingbox.x, this.boundingbox.y, this.boundingbox.width, this.boundingbox.height);
+        }
+        this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
     }
-    if (this.boxes) {
-        this.ctx.strokeStyle = "red";
-        this.ctx.strokeRect(this.x, this.y, this.animation.frameWidth, this.animation.frameHeight);
-        this.ctx.strokeStyle = "green";
-        this.ctx.strokeRect(this.boundingbox.x, this.boundingbox.y, this.boundingbox.width, this.boundingbox.height);
-    }
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
 }
 
 Explosion.prototype.update = function() {
