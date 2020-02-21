4540e47785aef554ca316a05c68c386d8502d1a1
Ken Gil Romero <kenromero12345@gmail.com>
2020-02-20 22:48:50 -0800

    Update explosion.js

--- explosion.js
+++ explosion.js
@@ -6,9 +6,20 @@ function Explosion(game, x, y) {
     this.speed = 200;
     this.x = x;
     this.y = y;
+    this.boxes = true;
+    this.boundingbox = new BoundingBox(this.x, this.y, this.width, this.height);
 }
 
 Explosion.prototype.draw = function() {
+    if (this.animation.isDone()) {
+        this.removeFromWorld = true;
+    }
+    if (this.boxes) {
+        this.ctx.strokeStyle = "red";
+        this.ctx.strokeRect(this.x, this.y, this.animation.frameWidth, this.animation.frameHeight);
+        this.ctx.strokeStyle = "green";
+        this.ctx.strokeRect(this.boundingbox.x, this.boundingbox.y, this.boundingbox.width, this.boundingbox.height);
+    }
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
 }
 
