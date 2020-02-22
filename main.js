16d626b1bec538f12f2613f1103531538618a211
Gordon Tran <51973793+trangordon@users.noreply.github.com>
2020-02-21 22:04:24 -0800

    turned off audio

--- main.js
+++ main.js
@@ -151,14 +151,16 @@ function Background(game, spritesheet) {
     this.spritesheet = spritesheet;
     this.game = game;
     this.ctx = game.ctx;
+    
 }
 
 Background.prototype.draw = function() {
     this.ctx.drawImage(this.spritesheet, this.x, this.y);
+    
 }
 
 Background.prototype.update = function() {
-    audio.play();
+    // audio.play();
 }
 
 // var GAMEBOARD = [];
