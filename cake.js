47dac2c1197ac7622bcfbabeec01a32a51ce84f4
mzloria <mikeeloria@gmail.com>
2020-02-24 15:04:56 -0800

    Revert "Revert "Merge remote-tracking branch 'origin/newGordon' into michael""
    This reverts commit 4c1636017b70f12aec8f7a1b7b82cda9c6468e3b.

--- cake.js
+++ cake.js
@@ -2,7 +2,7 @@ function cake(game, spawnX, spawnY, scale) {
     this.lifeDeduction = 8;
     this.name = "cake";
     this.speed = 100;
-    this.hp = 200;//
+    this.hp = 100;//
     this.money = 50;
     this.animationWalkLeft = new Animation(AM.getAsset("./img/cake.png")
     , 0, 85, 96, 90, 5, .135, 5, true, scale, false);
