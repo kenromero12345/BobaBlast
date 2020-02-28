0f7a72563258386c03660840f8b3a4ee4bf7e7fb
mzloria <mikeeloria@gmail.com>
2020-02-28 13:22:47 -0800

    Fixed ricochet bug

--- boba.js
+++ boba.js
@@ -17,7 +17,7 @@ function boba(game, startX, startY, name, target, damage, speed, ricochetLevel, 
     this.poisonTimeAdder = 0;
     this.paralysisTimeAdder = 0;
     this.burnTimeAdder = 0;
-    this.ricochetCount = ricochetCount;
+    this.ricochetCount = 0;
     this.isFreeze = false;
     if(name ==='blue') {
         this.isFreeze = true;
